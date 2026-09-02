f617dc899f5427dc5a6bd08b1020e9581f9e1055
Edi Wijaya <ediwexter@gmail.com>
2026-09-02 13:31:55 +0700

    Update primitive_types state and solutions; add tuple destructuring examples and tests

--- exercises/04_primitive_types/primitive_types5.rs
+++ exercises/04_primitive_types/primitive_types5.rs
@@ -1,8 +1,11 @@
 fn main() {
-    let cat = ("Furry McFurson", 3.5);
+    let cat: (&str, f32) = ("Furry McFurson", 3.5);
 
     // TODO: Destructure the `cat` tuple in one statement so that the println works.
     // let /* your pattern here */ = cat;
+    // let name: &str = &cat.0;
+    // let age: &f32 = &cat.1;
+    let (name, age) = cat;
 
     println!("{name} is {age} years old");
 }
