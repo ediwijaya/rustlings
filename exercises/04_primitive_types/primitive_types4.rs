ca68ac20e2b501099428be9effa8716ae8e50bf0
Edi Wijaya <ediwexter@gmail.com>
2026-09-02 12:56:14 +0700

    Update primitive_types4 exercise with slice example and tests; enhance comments for clarity

--- exercises/04_primitive_types/primitive_types4.rs
+++ exercises/04_primitive_types/primitive_types4.rs
@@ -1,5 +1,10 @@
 fn main() {
     // You can optionally experiment here.
+    let a:[i32; 5] = [1, 2, 3, 4, 5];
+
+    // TODO: Get a slice called `nice_slice` out of the array `a` so that the test passes.
+    let nice_slice: &[i32] = &a[1..4];
+    println!("{:?}", &nice_slice);
 }
 
 #[cfg(test)]
@@ -9,7 +14,7 @@ mod tests {
         let a = [1, 2, 3, 4, 5];
 
         // TODO: Get a slice called `nice_slice` out of the array `a` so that the test passes.
-        // let nice_slice = ???
+        let nice_slice = &a[1..4];
 
         assert_eq!([2, 3, 4], nice_slice);
     }
