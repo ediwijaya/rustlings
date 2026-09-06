73d2a94ff295a9dd5daee76df78fcc9c01c8c2ab
Edi Wijaya <ediwexter@gmail.com>
2026-09-06 09:53:06 +0700

    Update strings exercises; implement string manipulation functions and enhance solutions

--- exercises/09_strings/strings3.rs
+++ exercises/09_strings/strings3.rs
@@ -1,13 +1,18 @@
 fn trim_me(input: &str) -> &str {
     // TODO: Remove whitespace from both ends of a string.
+    input.trim()
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There are multiple ways to do this.
+    let mut result: String = input.to_string();
+    result.push_str(" world!");
+    return result;
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons".
+    input.replace("cars", "balloons")
 }
 
 fn main() {
