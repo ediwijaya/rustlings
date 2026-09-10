884571248d6116eec0c08d56dbcee8c6976a22fa
Edi Wijaya <ediwexter@gmail.com>
2026-09-10 15:51:43 +0700

    Update quiz2 exercise; implement transformer function and enhance tests

--- exercises/12_options/options1.rs
+++ exercises/12_options/options1.rs
@@ -4,6 +4,11 @@
 // `hour_of_day` is higher than 23.
 fn maybe_ice_cream(hour_of_day: u16) -> Option<u16> {
     // TODO: Complete the function body.
+    if hour_of_day < 22 {
+        5
+    } else {
+        None
+    }
 }
 
 fn main() {
--- exercises/quizzes/quiz2.rs
+++ exercises/quizzes/quiz2.rs
@@ -27,7 +27,22 @@ mod my_module {
     use super::Command;
 
     // TODO: Complete the function as described above.
-    // pub fn transformer(input: ???) -> ??? { ??? }
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
+        let mut result: Vec<String> = Vec::new();
+        for (mut text, command) in input {
+            match command {
+                Command::Uppercase => result.push(text.to_uppercase()),
+                Command::Trim => result.push(text.trim().to_owned()),
+                Command::Append(x) => {
+                    for _ in 0..x {
+                        text.push_str("bar");
+                    }
+                    result.push(text);
+                },
+            }
+        }
+        result
+    }
 }
 
 fn main() {
@@ -37,7 +52,7 @@ fn main() {
 #[cfg(test)]
 mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
-    // use ???;
+    use super::my_module::transformer;
     use super::Command;
 
     #[test]
